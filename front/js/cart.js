31a89a09199d59afaedbd4f89ff4e3286126d49f
Sarah <sarah.elba@live.fr>
2022-02-19 20:15:23 +0100

    Affichage du contenu du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,8 @@
 // Initialisation du localstorage
 let itemsInLocalStorage = JSON.parse(localStorage.getItem('cartItems'));
+
+// Affichage du contenu du panier
+async function displayCart() {
+  const parser = new DOMParser();
+  const positionEmptyCart = document.getElementById("cart__items");
+  let cartArray = [];
